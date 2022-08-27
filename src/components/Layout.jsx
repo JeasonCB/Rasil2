81d2d93aeefa978a635716fd5ed9328e08d4fe9d
Jeason Campos <camposjeason@gmail.com>
2022-08-27 15:53:36 -0400

    1.6

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@
 export default function Layout ({ children }) {
   return (
     <>
-      <div className="bg-gray-900 h-screen w-full">
+      <div className="bg-gray-900 h-[640px] w-full">
         {children}
       </div>
     </>
